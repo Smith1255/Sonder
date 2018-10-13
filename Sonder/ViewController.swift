a0e694a81655324b0f72bb57445a260febe1b9cb
Andrew Smith <andy-smith@comcast.net>
2018-10-13 17:05:15 -0400

    Hello World

--- Sonder/ViewController.swift
+++ Sonder/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func buttonClick(sender: UIButton) {
+        helloWorldLabel.isHidden = false
+    }
 
 }
 
