780f906b4ce8c064be51b939d8ea836d4c705394
Andrew Smith <andy-smith@comcast.net>
2018-11-05 13:19:03 -0500

    Fixed button size, added sign in

--- Sonder/ViewController.swift
+++ Sonder/ViewController.swift
@@ -10,15 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var helloWorldLabel: UILabel!
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     }
-    @IBAction func buttonClick(sender: UIButton) {
-        helloWorldLabel.isHidden = !(helloWorldLabel.isHidden)
-    }
+
     
 }
 
